56507b4da878f94703a0e3a01becd17eb27d9f1d
Sergey Sokolovskiy <ssokolovskiy@bioeebeanie.bioeelocal>
2025-03-27 12:53:34 -0400

    synthesis of all blocks

--- dc/fsm_sync/fsm_sync.nl.v
+++ dc/fsm_sync/fsm_sync.nl.v
@@ -1,30 +1,31 @@
 /////////////////////////////////////////////////////////////
 // Created by: Synopsys DC Ultra(TM) in wire load mode
 // Version   : V-2023.12-SP1
-// Date      : Sun Mar 23 20:06:14 2025
+// Date      : Thu Mar 27 12:19:13 2025
 /////////////////////////////////////////////////////////////
 
 
-module fsm_sync ( clk, rst, rfin, sh_en, state );
-  input clk, rst, rfin, sh_en;
-  output state;
-  wire   state_pos, next_state_pos, state_neg, next_state_neg, sh_en_prev, n1,
-         n2, n3, n4, n5, n6;
+module fsm_sync ( clk, rst, rfin, sh_en, fsm_rst, rfin_sync, state );
+  input clk, rst, rfin, sh_en, fsm_rst;
+  output rfin_sync, state;
+  wire   state_pos, N11, state_neg, N13, sh_en_prev, n7, n8, n9, n10, n11, n12,
+         n13;
 
-  UDB116SVT24_FDPRBQ_V2_1 state_pos_reg ( .D(next_state_pos), .CK(clk), .RD(n6), .Q(state_pos) );
-  UDB116SVT24_FDNRBQ_V2_1 state_neg_reg ( .D(next_state_neg), .CK(clk), .RD(n6), .Q(state_neg) );
-  UDB116SVT24_ND2_MM_0P75 U6 ( .A1(next_state_pos), .A2(n3), .X(n2) );
-  UDB116SVT24_OAI21B_1 U7 ( .A1(next_state_pos), .A2(rfin), .B(state_pos), .X(
-        n1) );
-  UDB116SVT24_ND2_MM_0P75 U9 ( .A1(next_state_neg), .A2(n3), .X(n5) );
-  UDB116SVT24_OAI21B_1 U10 ( .A1(rfin), .A2(next_state_neg), .B(state_neg), 
-        .X(n4) );
-  UDB116SVT24_FDPRBQ_V2_1 sh_en_prev_reg ( .D(sh_en), .CK(clk), .RD(n6), .Q(
-        sh_en_prev) );
-  UDB116SVT24_INV_0P75 U12 ( .A(rst), .X(n6) );
-  UDB116SVT24_AOI21_1 U13 ( .A1(n2), .A2(n1), .B(rst), .X(next_state_pos) );
-  UDB116SVT24_AOI21_1 U14 ( .A1(n5), .A2(n4), .B(rst), .X(next_state_neg) );
-  UDB116SVT24_ND2B_1 U15 ( .A(sh_en), .B(sh_en_prev), .X(n3) );
-  UDB116SVT24_OR2_0P75 U16 ( .A1(state_neg), .A2(state_pos), .X(state) );
+  UDB116SVT24_FDPQ_V2_3 state_pos_reg ( .D(N11), .CK(clk), .Q(state_pos) );
+  UDB116SVT24_FDNQ_V2_1 state_neg_reg ( .D(N13), .CK(clk), .Q(state_neg) );
+  UDB116SVT24_FDPQB_1P5 sh_en_prev_reg ( .D(n7), .CK(clk), .QN(sh_en_prev) );
+  UDB116SVT24_OR2_0P75 U13 ( .A1(rst), .A2(n8), .X(n7) );
+  UDB116SVT24_INV_0P75 U14 ( .A(sh_en), .X(n8) );
+  UDB116SVT24_AOI21_0P75 U15 ( .A1(sh_en_prev), .A2(n8), .B(fsm_rst), .X(n11)
+         );
+  UDB116SVT24_INV_0P75 U16 ( .A(state_pos), .X(n9) );
+  UDB116SVT24_AOI22_1 U17 ( .A1(state_pos), .A2(n11), .B1(rfin), .B2(n9), .X(
+        n13) );
+  UDB116SVT24_NR2_0P75 U18 ( .A1(n13), .A2(rst), .X(N11) );
+  UDB116SVT24_INV_0P75 U19 ( .A(state_neg), .X(n10) );
+  UDB116SVT24_AOI22_1 U20 ( .A1(state_neg), .A2(n11), .B1(rfin), .B2(n10), .X(
+        n12) );
+  UDB116SVT24_NR2_0P75 U21 ( .A1(n12), .A2(rst), .X(N13) );
+  UDB116SVT24_ND2_MM_0P75 U22 ( .A1(n13), .A2(n12), .X(state) );
 endmodule
 
