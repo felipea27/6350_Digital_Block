2b7f7dae58a0cdacf45bc33aa6a8353e6de0ecf3
Sergey Sokolovskiy <ssokolovskiy@bioeebeanie.bioeelocal>
2025-04-22 00:24:14 -0400

    apr scripts

---
+++ apr/apr/TOPv2.dat/viewDefinition.tcl
@@ -0,0 +1,40 @@
+if {![namespace exists ::IMEX]} { namespace eval ::IMEX {} }
+set ::IMEX::dataVar [file dirname [file normalize [info script]]]
+set ::IMEX::libVar ${::IMEX::dataVar}/libs
+
+create_library_set -name lib_fast\
+   -timing\
+    [list ${::IMEX::libVar}/mmmc/gf22nspslogl24edl116f_FFG_0P88V_0P00V_0P00V_0P00V_M40C.lib]
+create_library_set -name lib_slow\
+   -timing\
+    [list ${::IMEX::libVar}/mmmc/gf22nspslogl24edl116f_SSG_0P72V_0P00V_0P00V_0P00V_125C.lib]
+create_rc_corner -name rc_fast\
+   -preRoute_res 1\
+   -postRoute_res 1\
+   -preRoute_cap 1\
+   -postRoute_cap 1\
+   -postRoute_xcap 1\
+   -preRoute_clkres 0\
+   -preRoute_clkcap 0\
+   -qx_tech_file ${::IMEX::libVar}/mmmc/rc_fast/qrcTechFile
+create_rc_corner -name rc_slow\
+   -preRoute_res 1\
+   -postRoute_res 1\
+   -preRoute_cap 1\
+   -postRoute_cap 1\
+   -postRoute_xcap 1\
+   -preRoute_clkres 0\
+   -preRoute_clkcap 0\
+   -qx_tech_file ${::IMEX::libVar}/mmmc/rc_slow/qrcTechFile
+create_delay_corner -name dc_fast\
+   -library_set lib_fast\
+   -rc_corner rc_fast
+create_delay_corner -name dc_slow\
+   -library_set lib_slow\
+   -rc_corner rc_slow
+create_constraint_mode -name mode_mission\
+   -sdc_files\
+    [list ${::IMEX::dataVar}/mmmc/modes/mode_mission/mode_mission.sdc]
+create_analysis_view -name view_fast_mission -constraint_mode mode_mission -delay_corner dc_fast
+create_analysis_view -name view_slow_mission -constraint_mode mode_mission -delay_corner dc_slow
+set_analysis_view -setup [list view_slow_mission] -hold [list view_fast_mission]
--- dc/TOP/report/con.sdc
+++ dc/TOP/report/con.sdc
@@ -5,7 +5,7 @@
 ###################################################################
 set sdc_version 2.0
 
-set_units -time ns -resistance kOhm -capacitance pF -voltage V -current mA
+#set_units -time ns -resistance kOhm -capacitance pF -voltage V -current mA
 set_max_fanout 4 [current_design]
 set_load -pin_load 0.004 [get_ports pkt_rec]
 set_load -pin_load 0.004 [get_ports MISO]
